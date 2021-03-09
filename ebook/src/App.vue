6b5c1efc224f0865bb128f54c14a6caf987dbeef
lirijon <lirijon98gmail.com>
2021-03-09 19:35:48 +0100

    Header Component On App.vue

--- ebook/src/App.vue
+++ ebook/src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="app">
+    <Header/>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header"
 export default {
   name: 'App',
   components: {
+    Header
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ ebook/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <HeaderUpper/>
+        <Navigation/>
+    </div>
+</template>
+
+<script>
+import HeaderUpper from "./HeaderUpper";
+import Navigation from "./Navigation";
+export default{
+    name:"Header",
+    components:{
+        HeaderUpper,
+        Navigation
+    }
+
+}
+</script>
