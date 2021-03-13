d5ef55f7753fce6fe3e2bf1685a6fcb5c8a396c1
Alban salihu <albi.imi@live.com>
2021-03-13 15:40:20 +0100

    Offer Book Component

---
+++ ebook/src/components/OfferBook.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="first-offer">
+        <div class="offer-pic">
+            <img src="../assets/book12.png" alt="Photo">
+        </div>
+        <div class="name-price">
+            <div class="name-div">
+                <div class="name">
+                    Name
+                </div>
+                <div class="rating">
+                    <i>Autori:</i>
+                    Autori
+                </div>
+            </div>
+            <div class="name-div">
+                <p>
+                    <s>$40.48</s>
+                </p>
+                <p class="price">
+                    Price
+                </p>
+            </div>
+            <div class="name-div">
+                <div class="offer-time">
+                    <i class="fa fa-clock-o"></i> 1969 Days
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- ebook/src/pages/Home.vue
+++ ebook/src/pages/Home.vue
@@ -7,35 +7,7 @@
                 <div class="title">
                     HOT DEALS
                 </div>
-                <div class="first-offer">
-                    <div class="offer-pic">
-                        <img src="../assets/book12.png" alt="Photo">
-                    </div>
-                    <div class="name-price">
-                        <div class="name-div">
-                            <div class="name">
-                                Name
-                            </div>
-                            <div class="rating">
-                                <i>Autori:</i>
-                                Autori
-                            </div>
-                        </div>
-                        <div class="name-div">
-                            <p>
-                                <s>$40.48</s>
-                            </p>
-                            <p class="price">
-                                Price
-                            </p>
-                        </div>
-                        <div class="name-div">
-                            <div class="offer-time">
-                                <i class="fa fa-clock-o"></i> 1969 Days
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <OfferBook/>
                 <div class="title">
                     NEW ARRIVALS
                 </div>
@@ -160,10 +132,12 @@
 
 <script>
 import Book from "../components/Book"
+import OfferBook from "../components/OfferBook"
 export default {
     name: "Home",
     components:{
-        Book
+        Book,
+        OfferBook
     }
 }
 </script>
