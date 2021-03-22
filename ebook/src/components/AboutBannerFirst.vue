183674e76699a7def88ffc9796d58191c957d661
lirijon <lirijon98gmail.com>
2021-03-22 20:12:36 +0100

    changing structures and css in AboutBannerFirst

--- ebook/src/components/AboutBannerFirst.vue
+++ ebook/src/components/AboutBannerFirst.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="ourmission">
+        <div class="ourMissionImg">
             <img src="../assets/funnylaptop.jpg" />
-            <div class="ourmissiontext">
-                <h1>OUR MISSION</h1>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                   Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incid
-                </p>
-            </div>
         </div>
+
+        <div class="ourmissiontext">
+            <h2>OUR MISSION</h2>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incid
+            </p>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -24,17 +27,33 @@ export default {
 .ourmission{
     display:flex;
     flex-direction: row;
+    align-items: center;
+    flex-wrap: wrap;
 }
-
+.ourmission .ourMissionImg{
+    width: 50%;
+}
+.ourmission .ourMissionImg img{
+    width: 100%;
+}
 .ourmissiontext{
-    margin: 40px 20px 20px 20px;
-    padding: 5px;
+    width: 50%;
+    padding: 10px 40px;
 }
-.ourmissiontext h1{
+.ourmissiontext h2{
     margin-bottom: 40px;
 }
 .ourmissiontext p{
     line-height: 30px;
+}
+@media only screen and (max-width: 1240px){
+    .ourmission .ourMissionImg{
+        width: 100%;
+    }
+    .ourmissiontext{
+        width:100%;
+       
+    }   
 }
 
 </style>
