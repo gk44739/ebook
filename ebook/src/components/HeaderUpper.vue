e3a9b8747657bd564d495e1d899ae0e8f21cb650
Alban salihu <albi.imi@live.com>
2021-03-24 19:09:36 +0100

    Search Box Funcionality

--- ebook/src/components/HeaderUpper.vue
+++ ebook/src/components/HeaderUpper.vue
@@ -11,22 +11,26 @@
                     <div class="search">
                         <div class="all">
                             <div class="search-input">
-                                <input  type="text" placeholder="Search Books" id="searchBook">
+                                <input type="text" placeholder="Search Books" @focus="focus" @blur="blur" @keyup="search">
                             </div>
-                            <div class="search-icon">
+                            <div class="search-icon" @click="searchClick">
+                                <div class="click">
+                                </div>
+
                                 <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                                     viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-                                <g>
-                                    <path d="M225.474,0C101.151,0,0,101.151,0,225.474c0,124.33,101.151,225.474,225.474,225.474
-                                        c124.33,0,225.474-101.144,225.474-225.474C450.948,101.151,349.804,0,225.474,0z M225.474,409.323
-                                        c-101.373,0-183.848-82.475-183.848-183.848S124.101,41.626,225.474,41.626s183.848,82.475,183.848,183.848
-                                        S326.847,409.323,225.474,409.323z"/>
-                                </g>
-                                <g>
-                                    <path d="M505.902,476.472L386.574,357.144c-8.131-8.131-21.299-8.131-29.43,0c-8.131,8.124-8.131,21.306,0,29.43l119.328,119.328
-                                        c4.065,4.065,9.387,6.098,14.715,6.098c5.321,0,10.649-2.033,14.715-6.098C514.033,497.778,514.033,484.596,505.902,476.472z"/>
-                                </g>
+                                    <g>
+                                        <path d="M225.474,0C101.151,0,0,101.151,0,225.474c0,124.33,101.151,225.474,225.474,225.474
+                                            c124.33,0,225.474-101.144,225.474-225.474C450.948,101.151,349.804,0,225.474,0z M225.474,409.323
+                                            c-101.373,0-183.848-82.475-183.848-183.848S124.101,41.626,225.474,41.626s183.848,82.475,183.848,183.848
+                                            S326.847,409.323,225.474,409.323z"/>
+                                    </g>
+                                    <g>
+                                        <path d="M505.902,476.472L386.574,357.144c-8.131-8.131-21.299-8.131-29.43,0c-8.131,8.124-8.131,21.306,0,29.43l119.328,119.328
+                                            c4.065,4.065,9.387,6.098,14.715,6.098c5.321,0,10.649-2.033,14.715-6.098C514.033,497.778,514.033,484.596,505.902,476.472z"/>
+                                    </g>
                                 </svg>
+                                
                             </div>
                         </div>
                     </div>
@@ -44,6 +48,58 @@ export default {
     image: String,
     activeClass:Boolean
   },
+  methods:{
+    focus(){
+        if(this.$router.currentRoute.path === "/AllBooks"){
+            return;
+        }else{
+            this.$router.replace({path:"/AllBooks"})
+        }
+    },
+    blur(){
+        
+    },
+    search(e){
+        if(e.keyCode===13){
+            document.querySelectorAll(".book .name-price .name-div .name").forEach((element)=>{
+                if(element.innerText.toString().toLowerCase() === e.target.value.toString().toLowerCase()){
+                    document.querySelectorAll(".book .name-price .name-div .name").forEach((elementNone)=>{
+                        elementNone.parentNode.parentNode.parentNode.style.display="none";
+                    });
+                    element.parentNode.parentNode.parentNode.style.display="block";
+                }else{
+                    if(e.target.value.toString().toLowerCase() == ""){
+                        document.querySelectorAll(".book .name-price .name-div .name").forEach((elementBlock)=>{
+                            elementBlock.parentNode.parentNode.parentNode.style.display="block";
+                        });
+                    }
+                }
+            });
+        } 
+        if(e.target.value.toString().toLowerCase() == ""){
+            document.querySelectorAll(".book .name-price .name-div .name").forEach((element)=>{
+                element.parentNode.parentNode.parentNode.style.display="block";
+            });
+        }
+    },
+    searchClick(e){
+        document.querySelectorAll(".book .name-price .name-div .name").forEach((element)=>{
+            if(element.innerText.toString().toLowerCase() === e.target.parentNode.previousSibling.children[0].value.toString().toLowerCase()){
+                document.querySelectorAll(".book .name-price .name-div .name").forEach((elementNone)=>{
+                    elementNone.parentNode.parentNode.parentNode.style.display="none";
+                });
+                element.parentNode.parentNode.parentNode.style.display="block";
+            }else{
+                if(e.target.parentNode.previousSibling.children[0].value == ""){
+                    document.querySelectorAll(".book .name-price .name-div .name").forEach((elementBlock)=>{
+                        elementBlock.parentNode.parentNode.parentNode.style.display="block";
+                    });
+                }
+            }
+        });
+    }
+
+  }
   
 }
 </script>
@@ -118,7 +174,12 @@ export default {
 .search-icon:hover{
     fill: #e94c37;
 }
-
+.search-icon .click{
+    display: block;
+    position: absolute;
+    width: 20px;
+    height: 20px;
+}
 .search-icon{
     position: absolute;
     top: 12px;
